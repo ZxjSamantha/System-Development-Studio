cb6988ecfc984b0f9dd201cfd206c64fc994507b
ZxjSamantha <35060339+ZxjSamantha@users.noreply.github.com>
2020-11-20 14:42:31 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -ex
+
+target_branch="ghp-deploy"
+git config --global user.name "CircleCI deployer"
+git config --global user.email "<>"
+git checkout $target_branch
+git reset --hard origin/main
+
+gcc -o a.out a.c
+echo "output of a.out: $(./a.out)" > a.txt
+
+git add a.out a.txt
+git commit -m "[skip ci] updates GitHub Pages"
+if [ $? -ne 0 ]; then
+  echo "nothing to commit"
+  exit 0
+fi
+git remote set-url origin "https://github.com/ZxjSamantha/System-Development-Studio.git"
+git push -f origin $target_branch
