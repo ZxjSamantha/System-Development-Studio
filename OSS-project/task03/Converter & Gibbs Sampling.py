ee5fdbe1cf277aa188825628fb2f699416f61415
ZxjSamantha <35060339+ZxjSamantha@users.noreply.github.com>
2020-11-30 09:21:09 +0900

    Create Converter & Gibbs Sampling.py

---
+++ OSS-project/task03/Converter & Gibbs Sampling.py
@@ -0,0 +1,1 @@
+# Here is the source code of task 3: Make a converter to compute the weight matrix of the given states of neurons from the energy function. Use Gibbs sampling to update the RNN and check if N(x_1, x_2, x_3) follows Boltzmann distribution.
